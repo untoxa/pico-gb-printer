3f42b4c9ad0f46da40b8f843d57399c5ee794f7b
Cristofer Cruz <cristofer.cruz@gmail.com>
2022-09-16 17:12:49 -0500

    update content-type

--- include/lwipopts.h
+++ include/lwipopts.h
@@ -69,6 +69,6 @@
 
 #define LWIP_SINGLE_NETIF               1
 
-#define HTTPD_ADDITIONAL_CONTENT_TYPES {"bin", HTTP_CONTENT_TYPE("application/pico-gb-printer")}
+#define HTTPD_ADDITIONAL_CONTENT_TYPES {"bin", HTTP_CONTENT_TYPE("application/x-pico-printer-binary-log")}
 
 #endif /* __LWIPOPTS_H__ */
