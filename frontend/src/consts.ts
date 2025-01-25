5876275cb3f45139c9f21fd4a0bedfdd8869c587
Andreas Hahn <zatacke@gmail.com>
2025-01-25 16:23:48 +0100

    Removed names of unused endpoints

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,10 +1,6 @@
 // Api URLs
-export const OPTIONS = "/options";
 export const DOWNLOAD = "/download";
-export const RESET = "/reset";
-export const RESET_USB_BOOT = "/reset_usb_boot";
 export const STATUS_FILE = "/status.json"
-export const LIST_FILE = "/list.json"
 
 // Printer Commands
 export const COMMAND_INIT      = 0x01;
