4ef564e7e9359d9694eb4e6596fb43a38286f725
Andreas Hahn <zatacke@gmail.com>
2025-01-26 17:13:06 +0100

    Reduced Maximum Delay

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -15,7 +15,7 @@ export const TILE_SIZE     = 0x10;
 export const TILE_HEIGHT   = 8;
 export const TILE_WIDTH    = 8;
 
-export const MAX_POLL_DELAY = 2000;
+export const MAX_POLL_DELAY = 1500;
 export const BASIC_POLL_DELAY  = 10;
 
 export const LOCALSTORAGE_SCALE_KEY = 'pico-printer-save-scale';
